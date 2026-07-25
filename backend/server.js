ffbe5dc6dc0edf47ba6460df73ed3645d63a8989
TD <TD@ADMIN-PC>
2026-07-25 08:39:30 +0700

    sửa lại code

--- backend/server.js
+++ backend/server.js
@@ -7,15 +7,12 @@ dotenv.config();
 
 // 2. Import hàm connectDB từ file config/db.js
 const connectDB = require('./config/db');
-<<<<<<< HEAD
-=======
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const bloodRoutes = require('./routes/bloodRoutes');
 const urgentNewsRoutes = require('./routes/urgentNewsRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 const errorHandler = require('./middlewares/errorHandler');
->>>>>>> c68c433 (sửa lại code)
 
 const app = express();
 
@@ -30,8 +27,6 @@ app.get('/', (req, res) => {
   res.send('API BloodLink đang chạy!');
 });
 
-<<<<<<< HEAD
-=======
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/blood', bloodRoutes);
@@ -39,7 +34,6 @@ app.use('/api/urgent-news', urgentNewsRoutes);
 app.use('/api/admin', adminRoutes);
 app.use(errorHandler);
 
->>>>>>> c68c433 (sửa lại code)
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server đang chạy trên port: ${PORT}`);
